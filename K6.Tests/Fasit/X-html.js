5eda8159515ea76cf8c3408afda3e1a8e5dc18d9
kristiansam <samuelsen.kristian@gmail.com>
2022-09-08 16:28:45 +0200

    Update X-html.js

--- K6.Tests/Fasit/X-html.js
+++ K6.Tests/Fasit/X-html.js
@@ -1,4 +1,19 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  duration: '1m',
+  vus: 30
+}
+
+export default function () {
+  let res = http.get('https://localhost:7999/');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
 
 export function handleSummary(data) {
   return {
